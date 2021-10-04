1dd822477fc359b9cff2bd5f9c7594741b98b04e
ZacharyGillmore <88333868+ZacharyGillmore@users.noreply.github.com>
2021-10-03 17:31:02 -0700

    added turns and ability to win

--- main.lua
+++ main.lua
@@ -2,6 +2,8 @@
 win_w = 650 --window width
 win_h = 650 --windo hight
 letters = {'a','b','c','d','e','f','g','h'}
+turn = "WHITE"
+game_over = false
 function setUpBoard()
   --[[initilizes each space on the chess board--]]
   board = {
@@ -34,6 +36,55 @@ function setUpBoard()
   end
 end
 
+function isCheck(c)
+  local x,y
+  if color == "WHITE" then
+    for i = 1,8 do
+      for j = 1,8 do
+        if board[letters[j] .. i] == "K" then
+          x = j
+          y = i
+        end
+      end
+    end
+    for i = 1,8 do
+      for j = 1,8 do
+        fp = board[letters[j] .. i]
+        if fp ~= " "  and fp == fp:lower() then
+          moves = getPossibleMoves(j,i,"BLACK")
+          for k,v in pairs(moves) do
+            if v[1] == x and v[2] == y then
+              return true
+            end
+          end
+        end
+      end
+    end
+  else
+    for i = 1,8 do
+      for j = 1,8 do
+        if board[letters[j] .. i] == "k" then
+          x = j
+          y = i
+        end
+      end
+    end
+    for i = 1,8 do
+      for j = 1,8 do
+        fp = board[letters[j] .. i]
+        if fp ~= " "  and fp == fp:lower() then
+          moves = getPossibleMoves(j,i,"WHITE")
+          for k,v in pairs(moves) do
+            if v[1] == x and v[2] == y then
+              return true
+            end
+          end
+        end
+      end
+    end
+  end
+  return false
+end
 function checkSpace(x,y,c)
   --[[checks if a space is empty or has an enemy in it
   and that an that is in range of the board--]]
@@ -368,7 +419,7 @@ end
 function love.mousepressed(x,y,button)
   --[[function runs when mousebutton is pressed.
   allows the player to select a piece and move it--]]
-  if button == 1 then
+  if button == 1  and game_over ~= true then
     local tmp_x,tmp_y = returnMouseSquare(x,y)
     if tmp_x ~= nil and board[letters[tmp_x] .. tmp_y] ~= ' ' and p_selected == false then
       p_selected = true
@@ -376,11 +427,22 @@ function love.mousepressed(x,y,button)
       sp_y = tmp_y
     elseif tmp_x ~= nil and p_selected == true then
       local fp = board[letters[sp_x] .. sp_y]
+      color = "WHITE"
+      if fp == fp:upper() then
+        color = "WHITE"
+      else
+        color = "BLACK"
+      end
       v_moves = getPossibleMoves(sp_x,sp_y,fp)
       for i,v in pairs(v_moves) do
-        if v[1] == tmp_x and v[2] == tmp_y then
+        if v[1] == tmp_x and v[2] == tmp_y  and color == turn then
           board[letters[sp_x] .. sp_y] = ' '
           board[letters[tmp_x] .. tmp_y] = fp
+          if fp == fp:upper() then
+            turn = "BLACK"
+          else
+            turn = "WHITE"
+          end
           break
         end
       end
@@ -395,11 +457,34 @@ end
 
 function love.update(dt)
   --[[runs every frame--]]
-  local m_x,m_y = love.mouse.getPosition()
-  local tmp_x,tmp_y = returnMouseSquare(m_x,m_y)
-  if tmp_x ~= nil and p_selected == false then
+  if game_over ~= true then
+    local m_x,m_y = love.mouse.getPosition()
+    local tmp_x,tmp_y = returnMouseSquare(m_x,m_y)
+    if tmp_x ~= nil and p_selected == false then
+      highlights = {}
+      highlights = getPossibleMoves(tmp_x,tmp_y,board[letters[tmp_x] .. tmp_y])
+    end
+    cond_a = true
+    for i = 1,8 do
+      for j = 1,8 do
+        if board[letters[j] .. i] == "K" then
+          cond_a = false
+        end
+      end
+    end
+    cond_b = true
+    for i = 1,8 do
+      for j = 1,8 do
+        if board[letters[j] .. i] == "k" then
+          cond_b = false
+        end
+      end
+    end
+    if cond_a or cond_b then
+      game_over = true
+    end
+  else
     highlights = {}
-    highlights = getPossibleMoves(tmp_x,tmp_y,board[letters[tmp_x] .. tmp_y])
   end
 end
 
@@ -410,6 +495,19 @@ function love.draw()
   local font = love.graphics.newFont("arial_narrow_7.ttf", 48/(80/r_size))
   love.graphics.setColor(0, 0, 0)
   love.graphics.rectangle("fill", r_size-6, r_size-6, r_size*8+12, r_size*8+12)
+  if turn == "WHITE" then
+    if game_over then
+      love.graphics.print("Black Wins!",font,win_h/3,r_size-r_size/1.5)
+    else
+      love.graphics.print("White's turn",font,win_h/3,r_size-r_size/1.5)
+    end
+  else
+    if game_over then
+      love.graphics.print("White wins!",font,win_h/3,r_size-r_size/1.5)
+    else 
+      love.graphics.print("Blacks's turn",font,win_h/3,r_size-r_size/1.5)
+    end
+  end
   for i = 1,8 do
     love.graphics.setColor(0, 0, 0)
     love.graphics.print(9-i,font,r_size-r_size/2,i*r_size+r_size/3)
